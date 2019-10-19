15821408ad999613d0f0754ff1196996635f6bf5
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-10-19 10:03:01 -0500

    added first test

---
+++ Test 1 ECC @ Laboratoria.playground/Pages/Swift.xcplaygroundpage/Contents.swift
@@ -0,0 +1,48 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//: ## Swift
+//: Y finalmente, unas preguntas sobre Swift. ¡Vamos!
+
+//: 1. Escribe un hola mundo en Swift
+//Respuesta:
+
+//: 2. Escribe un ejemplo de String Interpolation
+//Respuesta:
+
+//: 3. Declara un arreglo constante de tipo Double con los siguientes elementos: 1.0, 5.0, 33.3, 45.5
+//Respuesta:
+ 
+//: 4. Usando un ciclo for, itera sobre el arreglo de la pregunta 3 e imprime cada uno de los elementos del arreglo.
+//Respuesta:
+
+//: 5. Declara la enumeración llamada Planet y como casos ocupa los siguientes:
+//: - Mercury
+//: - Venus
+//: - Earth
+//: - Mars
+//Respuesta
+
+//: 6. Declara una constante llamada planet y asígnale un caso de la enumeración Planet (el caso que quieras)
+//Respuesta:
+
+//: 7. Utiliza una sentencia switch para imprimir un mensaje dependiendo del valor de la constante planet de la pregunta 6.
+//Respuesta:
+
+//: 8. Declara una función llamada ```haveRead()``` que tome como parámetro el arreglo de cadenas ```books``` y la cadena ```book``` y regrese un booleano. La idea de esta función es determinar si la cadena ```book``` se encuentra dentro de ```books```
+
+let books = ["The Fountainhead", "Atlas Shrugged", "Why Nations Fail", "The Principles of Scientific Managemente", "Factfulness", "Outliers", "El Existencialismo es un Humanismo", "The Old Man and the Sea", "El Arte de Pensar", "Six Easy Pieces", "Steve Jobs", "Energy and Civilization: A History", "A History of Judaism"]
+let book = "Outliers"
+
+//Respuesta:
+
+//: 9. Finalmente, crea una estructura llamada ```Person``` que tenga como atributos:
+//: - name: String
+//: - books: [String]
+//: y como método la función de la pregunta 8 sin el parámetro ```books```.
+//: Crea una instancia de ```Person``` e inicialízala con tu nombre y ocupa la constante ```books``` de la pregunta 8 como segundo parámetro. Finalmente, manda a llamar al método ```haveRead()``` y pásale como parámetro la constante ```book``` de la pregunta 8. El resultado debe ser un booleano.
+
+//Respuesta
+
+
---
+++ Test 1 ECC @ Laboratoria.playground/Pages/Teoría.xcplaygroundpage/Contents.swift
@@ -0,0 +1,59 @@
+//: [Previous](@previous)
+
+//: ## Teoría
+//: Bienvenida al hermoso mundo de la teoría. Contesta las siguientes preguntas.
+
+//:    1¿Qué hace el archivo ViewController.swift?
+/*
+Respuesta:
+ 
+ */
+
+//: 2. ¿Qué puedes hacer dentro del archivo Main.storyboard?
+/*
+Respuesta:
+ 
+ */
+
+//: 3. ¿Cuál es la diferencia entre IBOutlet y IBAction?
+/*
+Respuesta:
+ 
+ */
+
+//: 4. ¿Menciona al menos una ventaja al utilizar UIStackView?
+/*
+Respuesta:
+ 
+ */
+
+//: 5. ¿Cuál es la versión más actual de Swift y iOS?
+//: a) Swift: 4.0 iOS: 13
+//: b) Swift: 5.0 iOS: 10
+//: c) Swift: 4.0 iOS: 11
+//: d) Swift: 5.0 iOS: 13
+/*
+Respuesta:
+ 
+ */
+
+//: 6. ¿Qué tengo que hacer para que mi interfaz sea vean *bien* en todos los dispositivos (Por *bien* me refiero a que si pongo un botón en el centro de mi vista y al girarlo, ese botón se siga viendo en el centro)?
+//: - a) Diseñar una interfaz para cada dispositivo
+//: - b) Agregar constraints
+//: - c) Ninguna de las anteriores
+/*
+Respuesta:
+ 
+ */
+
+//: 7. Supón que tienes un UIButton dentro de una vista A y quieres hacer una transición a otra vista B al presionar ese botón. ¿Qué tienes que hacer si quieres que la vista B aparezca de derecha a izquierda?
+//: - a) Embeber la vista A dentro de un UINavigationController > Ctrl + Drag del botón a la vista B  > Método de presentación: Show
+//: - b) Embeber la vista B dentro de un UINavigationController > Ctrl + Drag del botón a la vista B  > Método de presentación: PresentModally
+//: - c) Ctrl + Drag del botón a la vista B  > Método de presentación: Show
+//: - d) Ctrl + Drag del botón a la vista B  > Método de presentación: PresentModally
+/*
+Respuesta:
+ 
+ */
+
+//: [Next](@next)
